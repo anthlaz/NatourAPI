faa8ee6a416853b008126054ff8c4e4c235b6a67
lazosa <lazosa@deakin.edu.au>
2022-02-25 13:19:29 +1100

    created get route handler to response with JSON tours data

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 // import express
 const express = require('express');
-
+const fs = require('fs');
 // declare our app var
 const app = express();
 
 // ROUTES
-app.get('/', (req, res) => {
-  // callback function that will be executed when this route is hit
-  res.status(200).json({ message: 'Hello from the server.', app: 'Natours' });
+// app.get('/', (req, res) => {
+//   // callback function that will be executed when this route is hit
+//   res.status(200).json({ message: 'Hello from the server.', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint....');
+// });
+
+// read the tours data and parse it into a JSON object
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  // this callback is usually called the ROUTE HANDLER
+  // So what happens when the endpoint gets hit?
+  // 1. We need to get the data (but not in the callback function)
+  // 2. Send it back to the client with an added success data point
+  res
+    .status(200)
+    .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint....');
-});
 // SERVER
 const port = 8000;
 app.listen(port, () => {
